78abfb66cc8d471b6f0ddd08052c5e1ebad15a22
mananrg <gandhimanan1@gmail.com>
2024-03-11 22:39:25 -0400

    updated test_compare.py file

--- 2.multiple_file_execution/test_compare.py
+++ 2.multiple_file_execution/test_compare.py
@@ -1,23 +1,23 @@
 import pytest
 # Test 1
 # Test if num is greater than 100
-@pytest.mark.xfail
-@pytest.mark.great
+# @pytest.mark.xfail
+# @pytest.mark.great
 def test_greater():
     num = 100
     assert num > 100
 # Test 2
 # Test if num is greater than or equal to 100
-@pytest.mark.xfail
-@pytest.mark.great
+# @pytest.mark.xfail
+# @pytest.mark.great
 def test_greater_equal():
     num = 100
     assert num >= 100
 
 # Test 3
 # Test if num is less than 200
-@pytest.mark.skipif(True, reason="Test skipped because num is not less than 100")
-@pytest.mark.others
+# @pytest.mark.skipif(True, reason="Test skipped because num is not less than 100")
+# @pytest.mark.others
 def test_less():
     num = 100
     assert num < 100
